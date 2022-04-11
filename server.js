751eeac2466fdda8a250e3aad185d31ab1371915
charles johnston <charlesjohnston2002@yahoo.com>
2022-04-11 18:42:40 -0400

    setting up mysql connection

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "MysteryMachine$22",
+    database: "cj_employee_trackerDB"
+  });
+
