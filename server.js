7796cb36879d60e3ba9bbb84d198bb6a82312647
charles johnston <charlesjohnston2002@yahoo.com>
2022-04-11 18:44:39 -0400

    updating server on connection

--- server.js
+++ server.js
@@ -10,3 +10,21 @@ var connection = mysql.createConnection({
     database: "cj_employee_trackerDB"
   });
 
+  // this updates the server on connection
+function updateServer() {
+    connection.query("SELECT * from role", function(error, res) {
+      allroles = res.map(role => ({ name: role.title, value: role.id }));
+    });
+  
+    connection.query("SELECT * from department", function(error, res) {
+      alldepartments = res.map(dept => ({ name: dept.name, value: dept.id }));
+    });
+  
+    connection.query("SELECT * from employee", function(error, res) {
+      allemployees = res.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+      }));
+    });
+  }
+
