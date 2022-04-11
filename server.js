8079d543b55b4f02951f05366dcf7fe81b642828
charles johnston <charlesjohnston2002@yahoo.com>
2022-04-11 18:46:44 -0400

    startEmployeeManager function

--- server.js
+++ server.js
@@ -35,3 +35,58 @@ function updateServer() {
     updateServer();
   });
 
+
+function startEmployeeManager() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Employee",
+          "Add Department",
+          "Add Role",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+          case "View All Employees":
+            viewAllEmployees();
+            break;
+  
+          case "View All Departments":
+            viewAllDepartments();
+            break;
+  
+          case "View All Roles":
+            viewAllRoles();
+            break;
+  
+          case "Add Employee":
+            addEmployee();
+            break;
+  
+          case "Add Department":
+            addDepartment();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
+  
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+  
+          case "Exit":
+            connection.end();
+            break;
+        }
+      });
+  }
+
